df8e6f0844563e50ab06caa2f3beb3ed167405e2
vincentjulindra <vincentjulindrajj@gmail.com>
2025-07-17 15:07:06 +0000

    Update

--- app/Http/Controllers/EncryptionController.php
+++ app/Http/Controllers/EncryptionController.php
@@ -9,17 +9,21 @@ class EncryptionController extends Controller
     public function encryptData(Request $request)
     {
         // Kunci lemah untuk AES-256-CBC
-        $weakKey = '1234567890123456';  // Kunci lemah
-        $iv = '1234567890123456';  // IV yang lemah (16 bytes)
-
+        // $weakKey = '1234567890123456';  // Kunci lemah
+        // $iv = '1234567890123456';  // IV yang lemah (16 bytes)
+        
+        $key = 'GANTIp4k3y4ngsusah!!';
+        
         // Data yang akan dienkripsi
         $data = 'Sensitive Data';
 
         // Enkripsi menggunakan AES-256-CBC dengan kunci lemah
-        $cipher = openssl_encrypt($data, 'AES-256-CBC', $weakKey, 0, $iv);
-
+        // $cipher = openssl_encrypt($data, 'AES-256-CBC', $weakKey, 0, $iv);
+        $cipher = openssl_encrypt($data,'AES-256-CBC', $key, 0, $iv);
+        
         return response()->json([
-            'encrypted_data' => $cipher
+            'encrypted_data' => $cipher,
+            'iv' => base64_encode($iv)
         ]);
     }
 }
